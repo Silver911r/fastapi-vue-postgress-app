bddb09faf5432fc1418ac66fb306bae282575881
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-19 11:00:57 -0500

    add vuex state managment

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -4,13 +4,16 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 axios.defaults.withCredentials = true;
+// fastapi backend
 axios.defaults.baseURL = "http://localhost:5000/";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
