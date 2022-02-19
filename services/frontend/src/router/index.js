81deb0d2e85a04ffe484567f14caaa43437fbf37
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-19 14:45:51 -0500

    fix reading property null item

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Dashboard from "@/views/Dashboard.vue";
 import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
@@ -22,6 +23,12 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true },
   },
 ];
 
