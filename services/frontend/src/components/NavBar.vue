eb668b22343701333cedaddaf43de82410bd624d
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-19 12:38:35 -0500

    fix stray comma

--- services/frontend/src/components/NavBar.vue
+++ services/frontend/src/components/NavBar.vue
@@ -10,7 +10,6 @@
           data-bs-target="#navbarCollapse"
           aria-controls="navbarCollapse"
           aria-expanded="false"
-          ,
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
